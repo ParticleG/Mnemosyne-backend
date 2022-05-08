92a37520d8c5983bc2bae5035c835c7d984ff96e
ParticleG <particle_g@outlook.com>
2022-05-09 02:53:29 +0800

    - Add type `Visibility`

---
+++ types/Visibility.h
@@ -0,0 +1,15 @@
+//
+// Created by ParticleG on 2022/3/11.
+//
+
+#pragma once
+
+#include <drogon/drogon.h>
+
+namespace mnemosyne::types {
+    enum class Visibility {
+        Private,
+        Protected,
+        Public
+    };
+}
