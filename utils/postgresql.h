1675d4fa3a0799eb5a0efe48f94b5dcd64157a60
ParticleG <particle_g@outlook.com>
2022-05-10 03:16:38 +0800

    - Add `toCustomSql` method

--- utils/postgresql.h
+++ utils/postgresql.h
@@ -4,8 +4,17 @@
 
 #pragma once
 
+#include <drogon/orm/Criteria.h>
+#include <format>
 #include <helpers/RequestJson.h>
 
 namespace mnemosyne::utils::postgresql {
     std::string toPgArray(const helpers::RequestJson &requestJson);
+
+    template<typename... Arguments>
+    drogon::orm::CustomSql toCustomSql(const std::string &fmt, Arguments &&... arguments) {
+        return drogon::orm::CustomSql{
+            std::format(fmt, std::forward<Arguments>(arguments)...)
+        };
+    }
 }
